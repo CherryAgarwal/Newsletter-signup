95bf407b924bcc37b0fede97ddadda54818760bc
cherry <cherryagarwal1906@gmail.com>
2021-04-22 01:11:49 +0530

    commit done

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const app=express();
+const bodyParser = require('body-parser');
+const request = require('request');
+const https = require('https');
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({extended:true}));
+app.get("/",function(req,res){
+  res.sendFile(__dirname+'/index.html');
+
+});
+app.post("/",function(req,res){
+  const firstname=req.body.fname;
+  const lastname=req.body.lname;
+  const email=req.body.email;
+  const pass=req.body.password;
+
+  const data={
+    members:[
+      {
+        email_address : email,
+        status:"subscribed",
+        merge_fields:{
+          FNAME :firstname,
+          LNAME :lastname,
+
+        }
+      }
+    ]
+  };
+  const jsonData = JSON.stringify(data);
+  const url="https://us1.api.mailchimp.com/3.0/lists/4e6cb82200";
+  const options ={
+    method: "POST",
+    auth:"cherry1:2b5514e02eae0490a6bb7309704da3cf-us1"
+  }
+  const request=https.request(url,options,function(response){
+    if(response.statusCode === 200){
+      res.sendFile(__dirname + "/success.html");
+
+    }else{
+        res.sendFile(__dirname + "/failure.html");
+    }
+    response.on("data",function(data){
+      console.log(JSON.parse(data));
+    })
+  })
+  request.write(jsonData); request.end();
+});
+app.post("/failure",function(req,res){
+  res.redirect("/");
+
+});
+
+app.listen(process.env.PORT || 3000,function(){
+  console.log("Server is up and running");
+});
+//api key-2b5514e02eae0490a6bb7309704da3cf-us1
+//list id-4e6cb82200
